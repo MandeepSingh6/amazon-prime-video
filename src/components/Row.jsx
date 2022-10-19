59e8d38e8db0d5782113b5d19c1ee931fe6daca7
MandeepSingh6 <101950170+MandeepSingh6@users.noreply.github.com>
2022-10-19 01:16:18 -0400

    Update Row.jsx

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -3,12 +3,16 @@ import React from "react";
 const row = (props) => {
   return (
     <div className="">
-      <div className="w-52 bg-black text-white">
+      <div className="w-52 text-white relative group">
         <img
           className="w-full object-cover"
           src={`https://image.tmdb.org/t/p/original/${props.item?.backdrop_path}`}
           alt=""
         />
+        <div className="w-full top-0 left-0 absolute h-full bg-black opacity-75 hidden group-hover:block m-0"></div>
+        <div className="hidden absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-xs group-hover:block">
+          {props.item?.title}
+        </div>
       </div>
     </div>
   );
